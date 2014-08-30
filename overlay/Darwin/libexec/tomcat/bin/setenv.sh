fbff31cdf63b9b1a3aad5b03c8690f11a87dce73
Ralf Bensmann <ralf@art-of-coding.eu>
2014-08-30 18:57:54 +0200

    Tomcat setenv.sh: one script per OS, no memory defaults

---
+++ overlay/Darwin/libexec/tomcat/bin/setenv.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Common settings
+ODISEE_JVM_OPTS="-Djava.awt.headless=true -Dfile.encoding=UTF-8 -server"
+ODISEE_TOMCAT_OPTS="-Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Ddisable.auto.recompile=true"
+
+# Memory settings
+# -Xms -Xmx
+#ODISEE_MEM_OPTS="-Xms512m -Xmx512m"
+ODISEE_MEM_OPTS=""
+# Young generation
+# -XX:NewSize= -XX:MaxNewSize=
+#ODISEE_YOUNGEN_OPTS="-Xmn256m"
+ODISEE_YOUNGEN_OPTS=""
+# Permanent generation
+#ODISEE_PERMGEN_OPTS="-XX:PermSize=64m -XX:MaxPermSize=64m"
+ODISEE_PERMGEN_OPTS=""
+# Stack size
+#ODISEE_STACK_OPTS="-XX:ThreadStackSize=128k"
+ODISEE_STACK_OPTS=""
+
+# Error handling
+# OutOfMemoryError
+# -XX:OnOutOfMemoryError=\"\""
+ODISEE_OOM_ERROR="-XX:-HeapDumpOnOutOfMemoryError"
+# JVM error
+# -XX:OnError=\"<cmd args>;<cmd args>\""
+ODISEE_ON_ERROR=""
+
+# Performance optimization
+# -XX:+CMSClassUnloadingEnabled
+# -XX:+UseCompressedStrings -XX:+OptimizeStringConcat were introduced in Java 6u20 and u21
+ODISEE_OPTIM_OPTS="-XX:+UseStringCache -XX:+OptimizeStringConcat" 
+
+# Garbage Collection
+#GC_OPTS="-XX:+DisableExplicitGC"
+ODISEE_GC_OPTS=""
+#GC_DEBUG_OPTS="-Xloggc:$CATALINA_HOME/logs/gc.log -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
+ODISEE_GC_DEBUG_OPTS=""
+#GC_LOGROTATE="-XX:-UseGCLogRotation -XX:NumberOfGClogFiles=100 -XX:GCLogFileSize=1M"
+ODISEE_GC_LOGROTATE=""
+
+# Management
+#MGMT_OPTS="-Dcom.sun.management.jmxremote" 
+ODISEE_MGMT_OPTS=""
+
+# Extra options
+#
+# In case of ClassNotFoundException, e.g. [Ljava.lang.String; see Bug 6434149, http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6434149
+#-Dsun.lang.ClassLoader.allowArraySyntax=true
+ODISEE_EXTRA_OPTS=""
+
+JAVA_OPTS="$ODISEE_JVM_OPTS $ODISEE_MEM_OPTS $ODISEE_STACK_OPTS $ODISEE_YOUNGEN_OPTS $ODISEE_PERMGEN_OPTS $ODISEE_OPTIM_OPTS $ODISEE_TOMCAT_OPTS $ODISEE_MGMT_OPTS $ODISEE_GC_OPTS $ODISEE_GC_DEBUG_OPTS $ODISEE_OOM_ERROR $ODISEE_ON_ERROR $ODISEE_EXTRA_OPTS"
--- overlay/windows/libexec/tomcat/bin/setenv.bat
+++ overlay/windows/libexec/tomcat/bin/setenv.bat
@@ -1,32 +1,32 @@
-rem Common settings
+:: Common settings
 set JVM_OPTS=-Djava.awt.headless=true -Dfile.encoding=UTF-8 -server
 set TOMCAT_OPTS=-Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Ddisable.auto.recompile=true
 
-rem Memory settings
-rem -Xms -Xmx
+:: Memory settings
+:: -Xms -Xmx
 set MEM_OPTS= 
 set STACK_OPTS=-XX:ThreadStackSize=128k
-rem -XX:NewSize= -XX:MaxNewSize=
+:: -XX:NewSize= -XX:MaxNewSize=
 set YOUNGEN_OPTS=-Xmn256m
 set PERMGEN_OPTS=-XX:PermSize=64m -XX:MaxPermSize=64m
 
-rem Error handling
-rem -XX:OnOutOfMemoryError="<cmd args>"
+:: Error handling
+:: -XX:OnOutOfMemoryError="<cmd args>"
 set OOM_ERROR=-XX:-HeapDumpOnOutOfMemoryError
-rem ON_ERROR=-XX:OnError="<cmd args>;<cmd args>"
+:: ON_ERROR=-XX:OnError="<cmd args>;<cmd args>"
 set ON_ERROR=
 
-rem Performance optimization
-rem -XX:+CMSClassUnloadingEnabled
-rem -XX:+UseCompressedStrings -XX:+OptimizeStringConcat were introduced in Java 6u20 and u21
+:: Performance optimization
+:: -XX:+CMSClassUnloadingEnabled
+:: -XX:+UseCompressedStrings -XX:+OptimizeStringConcat were introduced in Java 6u20 and u21
 set OPTIM_OPTS="-XX:+UseStringCache -XX:+OptimizeStringConcat" # -XX:+UseCompressedStrings
 
-rem Garbage Collection
+:: Garbage Collection
 set GC_OPTS="-XX:+DisableExplicitGC"
 set GC_DEBUG_OPTS="-Xloggc:$CATALINA_HOME/logs/gc.log -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
 set GC_LOGROTATE="-XX:-UseGCLogRotation -XX:NumberOfGClogFiles=100 -XX:GCLogFileSize=1M"
 
-rem Management
+:: Management
 MGMT_OPTS="-Dcom.sun.management.jmxremote" 
 
 set JAVA_OPTS=%JVM_OPTS% %MEM_OPTS% %STACK_OPTS% %YOUNGEN_OPTS% %PERMGEN_OPTS% %OPTIM_OPTS% %TOMCAT_OPTS% %MGMT_OPTS% %GC_OPTS% %GC_DEBUG_OPTS% %OOM_ERROR% %ON_ERROR%
